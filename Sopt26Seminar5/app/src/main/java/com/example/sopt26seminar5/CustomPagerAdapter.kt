7f0363bd19da63ca50f42b18db9af6ec0fa4b376
jujube0 <60654009+jujube0@users.noreply.github.com>
2020-05-25 16:25:41 +0900

    mc_home

---
+++ Sopt26Seminar5/app/src/main/java/com/example/sopt26seminar5/CustomPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.soptseminar5
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class CustomPagerAdapter(fm:FragmentManager):
+FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
+
+    val images= arrayOf(
+        R.drawable.home_adpic01,
+        R.drawable.home_adpic01,
+        R.drawable.home_adpic01
+    )
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position){
+            0-> Adpic(images[0])
+            1-> Adpic(images[1])
+            else->Adpic(images[2])
+        }
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+
+}
